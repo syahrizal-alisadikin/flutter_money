d0154cbb7ddaf9df93b1e361097a126ea1505300
syahrizal-alisadikin <alisadikinsyahrizal@gmail.com>
2022-08-15 17:10:15 +0700

    input login

--- lib/pages/auth/login.dart
+++ lib/pages/auth/login.dart
@@ -4,9 +4,16 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:money_record/config/app_asset.dart';
 import 'package:money_record/config/app_color.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
 
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final controllerEmail = TextEditingController();
+  final controllerPassword = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,14 +23,74 @@ class LoginPage extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
               DView.nothing(),
-              Image.asset(AppAsset.logo),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 30),
+                child: Column(
+                  children: [
+                    Image.asset(AppAsset.logo),
+                    DView.spaceHeight(24),
+                    TextField(
+                      controller: controllerEmail,
+                      decoration: InputDecoration(
+                        fillColor: AppColor.primary.withOpacity(0.6),
+                        filled: true,
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(16),
+                          borderSide: BorderSide.none,
+                        ),
+                        hintText: "Masukan Email",
+                        // labelText: 'Enter your username',
+                        hintStyle: GoogleFonts.poppins(
+                          color: Colors.black,
+                          fontSize: 16,
+                          fontWeight: FontWeight.w400,
+                        ),
+                        isDense: true,
+                        contentPadding: EdgeInsets.symmetric(
+                          vertical: 12,
+                          horizontal: 16,
+                        ),
+                      ),
+                    ),
+                    DView.spaceHeight(16),
+                    TextField(
+                      controller: controllerPassword,
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        fillColor: AppColor.primary.withOpacity(0.6),
+                        filled: true,
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(16),
+                          borderSide: BorderSide.none,
+                        ),
+                        hintText: "Masukan Password",
+                        // labelText: 'Enter your username',
+                        hintStyle: GoogleFonts.poppins(
+                          color: Colors.black,
+                          fontSize: 16,
+                          fontWeight: FontWeight.w400,
+                        ),
+                        suffixIcon: Icon(
+                          Icons.visibility,
+                          color: Color(0xff6F7075),
+                        ),
+                        isDense: true,
+                        contentPadding: EdgeInsets.symmetric(
+                          vertical: 12,
+                          horizontal: 16,
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
               Padding(
                 padding: const EdgeInsets.fromLTRB(16, 16, 16, 20),
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
                     Text(
-                      'Belum punya akun?',
+                      'Belum punya akun nih?',
                       style: GoogleFonts.roboto(
                         fontSize: 16,
                         fontWeight: FontWeight.w400,
